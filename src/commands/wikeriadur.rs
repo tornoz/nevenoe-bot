09aa879dcdf0c7fb1a8c9db59dcaca4ec604c504
Anthony Guéchoum <a.guechoum@gmail.com>
2018-09-15 19:07:23 +0200

    WIP / correctly parse raw article from breton wiktionary

--- src/commands/wikeriadur.rs
+++ src/commands/wikeriadur.rs
@@ -1,29 +1,51 @@
-use html2runes::markdown;
 use reqwest;
 use regex::Regex;
 use std::error::Error;
 
 static WIKERIADUR_URL: &'static str = "https://br.wiktionary.org/wiki/%term%?action=raw";
-static langRegex = r"(?m){{=[a-z]{2,3}=}}";
-static classRegex = r"(?m){{-([[:alpha:]-]*)-\|[a-z]{2,3}.{0,}}}";
-static definitionRegex = r"(?m)^#[^*](.*)$";
-static exampleRegex = r"(?m)^#\*(.*)$";
+static langRegex: &'static str = r"(?m)\{\{=([a-z]{2,3})=\}\}";
+static classRegex: &'static str = r"(?m)\{\{-([[:alpha:]-]*)-\|[a-z]{2,3}.{0,}\}\}";
+static definitionRegex: &'static str = r"(?m)^#[^*](.*)$";
+static exampleRegex: &'static str = r"(?m)^#\*(.*)$";
 
 // pub fn run(term: &str) -> Result<String, &Error> {
 pub fn run(term: &str) -> String {
     let uri = str::replace(WIKERIADUR_URL, "%term%", &term);
+    let mut lang: &str;
+    let mut wordClass: &str;
+    let mut definition: &str;
+    let mut example: &str;
+
+    let langRe = Regex::new(langRegex).unwrap();
+    let classRe = Regex::new(classRegex).unwrap();
+    let definitionRe = Regex::new(definitionRegex).unwrap();
+    let exampleRe = Regex::new(exampleRegex).unwrap();
 
     println!("{}", &uri);
     let mut res = reqwest::get(&uri).unwrap();
-    let text = markdown::convert_string(&res.text().unwrap());
+    let text = &res.text().unwrap();
+    let mut lines:Vec<&str> = text.split("\n").collect();
 
-    let re = Regex::new(r"'''(.*)'''(.*)#(.*)#(.*)").unwrap();
+    for line in &lines {
+        //println!("LINE: {} ", line);
+        for cap in langRe.captures_iter(line) {
+            println!("LANG: {} ", &cap[1]);
+        }
 
-    // works with "gwalleur"
-    for cap in re.captures_iter(&text) {
-        println!("CAP1: {}, CAP2: {}, CAP3: {}, CAP4: {} ", &cap[1], &cap[2], &cap[3], &cap[4]);
+        for cap in classRe.captures_iter(line) {
+            println!("CLASS: {} ", &cap[1]);
+        }
+
+        for cap in definitionRe.captures_iter(line) {
+            println!("DEFINITION: {} ", &cap[1]);
+        }
+
+        for cap in exampleRe.captures_iter(line) {
+            println!("EXAMPLE: {} ", &cap[1]);
+        }
     }
+
     //println!("{}", after);
 
-    return text;
+    return "text".to_string();
 }
